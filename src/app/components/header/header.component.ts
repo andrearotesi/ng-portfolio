319fc0965a0364a1ee65320574683e20b588afb7
andrearotesidev <andrearotesi@gmail.com>
2023-02-25 16:13:51 +0100

    Added header styling

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'ptf-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
@@ -19,6 +20,13 @@ export class HeaderComponent implements OnInit {
     'How to center a div?',
     '!{false} - it\'s funny because it\'s true'
   ];
+
+  navItems = [
+    {
+      name: 'Home',
+      route: ''
+    }
+  ]
 
   ngOnInit(): void {
     this.randomMantra = this.mantras[Math.floor(Math.random() * this.mantras.length)];
